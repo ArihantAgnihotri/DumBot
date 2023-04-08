65f6050031890b9638f61776ce989c3d474c8ef0
Arihant Agnihotri <arihantagnihotri2106@gmail.com>
2023-04-09 02:30:08 +0530

    Add Bot token to env variables

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-# bot token : ODQ4OTg3MjQ4NzExMDQxMDk0.G74RkY.sxUH3NpCrHyfmcz2xR0puH6XEnHhhmO6-d3WNE
+
 # url : https://discord.com/api/oauth2/authorize?client_id=848987248711041094&permissions=8&scope=bot%20applications.commands
+import os
+from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+
+load_dotenv()
+token = os.getenv('TOKEN')
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
 
 @bot.event
 async def on_ready():
     print('Bot is online')
 
 
-bot.run("ODQ4OTg3MjQ4NzExMDQxMDk0.G74RkY.sxUH3NpCrHyfmcz2xR0puH6XEnHhhmO6-d3WNE")
+bot.run(token)
